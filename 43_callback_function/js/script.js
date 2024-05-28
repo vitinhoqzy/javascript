2690cabcdf1d392bf4984d99f0a467b80e63a140
vitinhoqzy <vi083450@gmail.com>
2024-05-28 00:29:55 -0300

    marcha no progresso

---
+++ 43_callback_function/js/script.js
@@ -0,0 +1,18 @@
+function exibir(num) {
+    console.log("A operacao resultou em " + num);
+}
+
+
+function soma(a, b, callback) {
+    var op = a + b;
+     callback(op);
+}
+
+function multiplicacao(a, b, cb) {
+    var op = a * b;
+    cb(op);
+}
+
+soma(2,2, exibir);
+
+multiplicacao(2,4, exibir);
---
+++ 44_setTimeout_setInterval/js/script.js
@@ -0,0 +1,17 @@
+// setTimeout
+console.log("antes do setTimeout");
+
+
+setTimeout(function(){
+     console.log("testando o setTimeout");
+
+},2000);
+
+console.log("depois do setTimeout");
+
+// setInterval
+
+/*setInterval(function(){
+  console.log("Testando setInterval");
+
+}, 1000);*/
